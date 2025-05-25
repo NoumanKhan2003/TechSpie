62d63c131d2030e1f25c542393f963933499e96f
Sajal439 <23001001107sajal@dcrustm.org>
2025-05-25 20:52:21 +0530

    created a work page to showcase our works

---
+++ client/src/Pages/WorkPage.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import { projectsData } from "../data/projectsData";
+import ProjectGrid from "../Components/works/ProjectGrid";
+import CtaSection from "../Components/works/CtaSection";
+import WorkHeader from "../Components/works/WorkHeader";
+
+const WorkPage = () => {
+  const [filter] = useState("All");
+
+  const filteredProjects =
+    filter === "All"
+      ? projectsData
+      : projectsData.filter((project) => project.category === filter);
+
+  return (
+    <div className="container mx-auto px-4 py-16 max-w-7xl bg-gradient-to-b from-gray-50 to-white min-h-screen">
+      <WorkHeader />
+      <ProjectGrid projects={filteredProjects} />
+      <CtaSection />
+    </div>
+  );
+};
+
+export default WorkPage;
