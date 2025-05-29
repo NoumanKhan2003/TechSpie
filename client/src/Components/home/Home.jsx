d6bbde0894d137d761e2f3883938e3da9e1784ea
Nouman Khan <noumankhan16859@gmail.com>
2025-05-29 18:44:12 +0530

    Home page bg icon removed

--- client/src/Components/home/Home.jsx
+++ client/src/Components/home/Home.jsx
@@ -97,12 +97,10 @@ const Home = () => {
           </div>
 
           {/* Scroll Indicator */}
-          <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 animate-bounce rotate-3">
             <div className="w-6 h-10 border-2 border-purple-200 rounded-full flex justify-center">
               <div className="w-1 h-3 bg-purple-300 rounded-full mt-2 animate-pulse"></div>
             </div>
           </div>
-        </div>
       </div>
     </main>
   );
