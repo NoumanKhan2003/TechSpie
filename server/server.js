aa55fb4e52daafd9a7b343229344076e03610f85
Nouman Khan <noumankhan16859@gmail.com>
2025-06-14 16:05:02 +0530

    basic ping route created

--- server/server.js
+++ server/server.js
@@ -1,1 +1,26 @@
-console.log("hello Nouman")
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import cors from "cors";
+import pingRoute from "./Routes/pingRoute.js";
+
+
+const app = express();
+dotenv.config();
+
+app.use(express.json());
+
+mongoose
+  .connect(process.env.Mongo_Url)
+  .then(() => {
+    console.log("Database connected");
+  })
+  .catch((error) => {
+    console.log("Error connecting to database", error);
+  });
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use('/',pingRoute);
+app.listen(process.env.PORT || 3000);
