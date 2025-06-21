8d10209ed8b7c96e5805ef16be66f02bf61f79c3
Nouman Khan <noumankhan16859@gmail.com>
2025-06-21 11:17:33 +0530

    Main app and css file corrected

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,29 +11,23 @@ import ContactUsPage from "./Pages/ContactUsPage.jsx";
 import WorkPage from "./Pages/WorkPage.jsx";
 import LoginPage from "./Pages/LoginPage.jsx";
 import { GoogleOAuthProvider } from "@react-oauth/google";
+
 function App() {
-  const GoogleAuthWrapper = () => {
-    return (
-      <GoogleOAuthProvider clientId={`${import.meta.env.VITE_GOOGLE_CLIENT_ID}`}>
-        <LoginPage></LoginPage>
-      </GoogleOAuthProvider>
-    );
-  };
   return (
-    <>
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
       <Navbar />
       <Routes>
-        <Route path="*" element={<ContactUsPage />} />
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutUs />} />
         <Route path="/services" element={<Service />} />
         <Route path="/team" element={<Team />} />
         <Route path="/contact" element={<ContactUsPage />} />
         <Route path="/work" element={<WorkPage />} />
-        <Route path="/login" element={<GoogleAuthWrapper />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="*" element={<ContactUsPage />} />
       </Routes>
       <Footer />
-    </>
+    </GoogleOAuthProvider>
   );
 }
 
